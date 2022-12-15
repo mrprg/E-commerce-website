82d267ba5f6cd8c0486a0f8923e739525ea77223
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-15 11:11:13 +0330

    styling the Navbar components with material-ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
+import { MenuItem } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from '@mui/material';
+// import { MailIcon }
 import React from 'react';
 import styled from 'styled-components';
 
@@ -11,18 +15,39 @@ const Wrapper = styled.div`
 `
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
 `
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
 `
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+    border: none;
+`
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
+`
+
+const Logo = styled.h1`
+    font-weight: bold:
 `
 const Right = styled.div`
     flex: 1;
-`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
 
 const Navbar = () => {
   return (
@@ -31,12 +56,20 @@ const Navbar = () => {
             <Left>
               <Language>EN</Language>
               <SearchContainer>
-                input
-                icon
+                <Input />
+                <Search style={{ color:'gray', fontSize:16 }} />
               </SearchContainer>
             </Left>
-            <Center>center</Center>
-            <Right>right</Right>
+            <Center><Logo>Asadi.</Logo></Center>
+            <Right>
+              <MenuItem>REGISTER</MenuItem>
+              <MenuItem>SIGN IN</MenuItem>
+              <MenuItem>
+                 <Badge badgeContent={4} color="primary">
+                      <ShoppingCartOutlined />
+                  </Badge> 
+              </MenuItem>
+            </Right>
             
         </Wrapper>
     </Container>
