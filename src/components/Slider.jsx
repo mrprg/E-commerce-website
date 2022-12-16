b6b17ac78e0221bcb27671ec4428df62237453f3
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-16 19:02:47 +0330

    add animation to slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,6 +34,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
+  transition: all 1.5s ease;
   transform: translateX(${(props) => props.slideIndex * -100}vh)
 `;
 
