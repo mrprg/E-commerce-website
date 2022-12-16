aa4ac0668f88b49545d72f4ff533d85b377d2de6
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-16 18:26:19 +0330

    data for slider

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/originals/b3/d4/78/b3d478247996a3a650ecb24a9f242f63.jpg",
+        title: "WINTER SALE!!",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/originals/79/a6/bb/79a6bb483e693c5f4c73130434478bc0.jpg",
+        title: "POPULAR SALE!!",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    },
+    {
+        id: 3,
+        img: "https://th.bing.com/th/id/R.130b1cad8d069c270b4831cd33a0f8d9?rik=rZSt055H%2fbBBVA&pid=ImgRaw&r=0",
+        title: "SUMMER SALE!!",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    },
+]
