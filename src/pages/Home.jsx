46a920c2203882e7b7670884afb936cad9699035
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-17 21:52:22 +0330

    add NewsLetter component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
 import Products from '../components/Products';
 import Slider from '../components/Slider';
 
@@ -13,6 +14,7 @@ const Home = () => {
         <Slider />
         <Categories />
         <Products />
+        <Newsletter />
     </div>
   )
 }
