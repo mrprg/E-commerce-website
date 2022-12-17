16d5559487dcd939d45dad897cd878817471a8af
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-17 19:08:45 +0330

    change image links

--- src/data.js
+++ src/data.js
@@ -22,17 +22,19 @@ export const sliderItems = [
 export const categories = [
     {
         id: 1,
-        img: "",
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title: "SHIRT STYLE!",
     },
     {
         id: 2,
-        img: "",
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title: "LOUNGEWEAR LOVE",
     },
     {
         id: 3,
-        img: "",
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         title: "LIGHT JACKETS",
     },
 ]
+
+// https://cdn.cliqueinc.com/posts/293485/summer-jackets-for-women-293485-1622148636742-main.700x0c.jpg
