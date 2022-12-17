8b3f8880a82cd785cbd26847f529a978e6e16e5c
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-17 19:09:10 +0330

    Style : styling the category component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 import CategoryItem from './CategoryItem';
 import { categories } from '../data';
 
-const Container = styled.div``
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`
 
 const Categories = () => {
   return (
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,10 +1,53 @@
 import styled from 'styled-components';
 
-const Container = styled.div``
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`;
+
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+`;
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+    border: none;
+    padding: 20px;
+    background-color: white;
+    color: gray;
+    cursor: pointer;
+    font-weight: 600;
+`;
 
 const CategoryItem = ({ item }) => {
   return (
-    <Container>CategoryItem</Container>
+    <Container>
+        <Image src= {item.img}/>
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+    </Container>
   )
 }
 
