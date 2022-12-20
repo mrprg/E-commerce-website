b2b1bafba22b604168986a5855b54bae2cdc9cb3
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-20 20:33:06 +0330

    new page: add productList page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
 import './App.css';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div className="App">
-        <Home />
+        <ProductList />
     </div>
   );
 }
