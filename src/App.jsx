3a166de0ea350093e9366dff846a59bc5b87f605
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-21 21:43:23 +0330

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from './pages/Home';
 import './App.css';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-        <Product />
+        <Login />
     </div>
   );
 }
