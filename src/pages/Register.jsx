2e028c96393ac17ae0b7ea88596efea6923d5ab3
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-21 21:50:31 +0330

    fix the style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 98vh;
   background: linear-gradient(
       rgba(255, 255, 255, 0.5),
       rgba(255, 255, 255, 0.5)
