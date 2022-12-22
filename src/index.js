de9d22ea6c465114122885006c603f471e9afdb0
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-22 10:19:27 +0330

    wrapping app.js with Stateprovider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StateProvider } from './context/Stateprovider';
+import reducer, { initialState } from './context/reducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
