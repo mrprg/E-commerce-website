643425ab9fb7b6c42acc9a3d3ab94497377d0bea
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-23 21:31:55 +0330

    fix the link

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 
@@ -48,7 +49,9 @@ const CategoryItem = ({ item }) => {
       <Image src={item.img} />
       <Info>
         <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
+        <Link to='/productlist'>
+          <Button>SHOP NOW</Button>
+        </Link>
       </Info>
     </Container>
   );
